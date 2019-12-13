7b743a86ea6022b54555ff2aa3d11f096eeaeef1
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2019-12-12 18:12:48 -0800

    Initial commit

---
+++ src/components/stopwatch.js
@@ -0,0 +1,69 @@
+import React, {Component} from "react";
+
+class Stopwatch extends Component {
+  state = {
+    holdingSpaceAtStop: false,
+    isTiming: false,
+    runningTime: 0
+  };
+
+  componentDidMount() {
+    document.body.onkeyup = this.handleOnKeyUp;
+    document.body.onkeypress = this.handleKeyDown;
+  }
+
+  handleOnKeyUp = e => {
+    if (e.key === " ") {
+      if (!this.state.isTiming && !this.state.holdingSpaceAtStop) {
+        this.setState(state => {
+          const startTime = Date.now();
+          this.timer = setInterval(() => {
+            this.setState({runningTime: Date.now() - startTime});
+          }, 10);
+        });
+
+        this.setState({isTiming: true});
+      }
+
+      // Prevents stopwatch from starting again after finishing
+      this.setState({holdingSpaceAtStop: false});
+    }
+  }
+
+
+  handleKeyDown = (e) => {
+    if (e.key === " ") {
+      if (this.state.isTiming) {
+        clearInterval(this.timer);
+        this.setState({isTiming: false, holdingSpaceAtStop: true});
+      }
+    }
+
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  displayStopwatch() {
+    const seconds = Math.trunc(this.state.runningTime / 1000);
+    const minutes = Math.trunc(seconds / 60);
+
+    let centiseconds = Math.trunc(this.state.runningTime / 10) % 100;
+    // Add leading zeros
+    centiseconds = ("0" + centiseconds).substr(-2);
+
+    return minutes === 0 ? `${seconds}.${centiseconds}` : `${minutes}:${seconds}.${centiseconds}`;
+  }
+
+
+  render() {
+    return (
+      <div onKeyUp={e => console.log(e.keyCode)}>
+        <p>{this.displayStopwatch()}</p>
+      </div>
+    );
+  }
+}
+
+export default Stopwatch;
