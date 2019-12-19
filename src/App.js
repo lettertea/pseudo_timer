4a191600b56fe50c155c868ee28d3e782364e5b1
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2019-12-18 18:24:13 -0800

    Add scrambles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Stopwatch from "./components/Stopwatch";
 import Times from "./components/Times";
+import {ICAScrambo} from "icascrambo/dist/ICAScrambo";
+
+const scrambler = new ICAScrambo();
 
 function App() {
+
   const [recordedTimes, setRecordedTimes] = useState([]);
+  const [scramble, setScramble] = useState("");
+
+  useEffect(() => {
+    setScramble(scrambler.get()[0].scramble_string);
+  }, [recordedTimes]);
+
   return (
     <div className="App">
-      <Stopwatch recordedTimes = {recordedTimes} setRecordedTimes = {setRecordedTimes}/>
-      <Times recordedTimes = {recordedTimes}  />
+      {scramble}
+      <Stopwatch recordedTimes={recordedTimes} setRecordedTimes={setRecordedTimes}/>
+      <Times recordedTimes={recordedTimes}/>
     </div>
   );
 }
