9f8c9675c548b59fa6e9e59b09ae15f6fed31210
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2020-01-12 18:22:57 -0800

    Remove stateful logic in App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,43 @@
-import React, {Component, createRef} from "react";
+import React, { Component, createRef } from "react";
 import Stopwatch from "./components/Stopwatch";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import BottomNav from "./components/BottomNav";
 import StyledPaper from "./components/StyledPaper";
-import {connect} from "react-redux";
 import Scramble from "./components/Scramble";
-import {bindActionCreators} from "redux";
-import {setTimes, updateScramble} from "./actions";
-
 
 class App extends Component {
-  state = {
-    recordedTimes: {},
-    scrambledCubeSvg: "",
-    scaleFactor: 2
-  };
-
   svgRef = createRef();
 
   render() {
     return (
-      <Container style={{
-        marginTop: "10vh"
-      }}>
+      <Container
+        style={{
+          marginTop: "10vh"
+        }}
+      >
         <Grid container direction="column" alignItems={"center"}>
-
-          <Stopwatch/>
-          <Scramble/>
-
+          <Stopwatch />
+          <Scramble />
 
           <Grid container spacing={4}>
             <Grid item xs={8}>
-              <BottomNav
-                recordedTimes={this.state.recordedTimes[this.props.wcaEvent]}
-                wcaEvent={this.props.wcaEvent}
-                setWcaEvent={value => this.setState({wcaEvent: value})}
-              />
+              <BottomNav />
             </Grid>
             <Grid item xs={4}>
               {/* Hardcode margin top to align with contents of BottomNav */}
               {/* Probably not best practice, but I'm getting lazy. */}
-              <StyledPaper height={340} style={{
-                marginTop: "56px",
-              }}>
+              <StyledPaper
+                height={340}
+                style={{
+                  marginTop: "56px"
+                }}
+              >
                 <div
                   ref={this.svgRef}
                   style={{
                     transform: `scale(${this.props.svgScale})`,
-                    transformOrigin: "center center",
+                    transformOrigin: "center center"
                   }}
                 />
               </StyledPaper>
@@ -60,10 +49,4 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  svgScale: state.svgScale,
-  wcaEvent: state.wcaEvent,
-})
-export default connect(
-  mapStateToProps
-)(App)
+export default App;
