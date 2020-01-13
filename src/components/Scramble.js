8f3c06c7415c301bdb05f1edbe51edfccb9a22ca
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2020-01-12 17:55:01 -0800

    Convert Scramble to class component

--- src/components/Scramble.js
+++ src/components/Scramble.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import Typography from "@material-ui/core/Typography";
 import { connect } from "react-redux";
 
-function Scramble(props) {
-  return (
-    <Typography
-      variant={"body1"}
-      color={"textSecondary"}
-      style={{ marginBottom: 80 }}
-    >
-      {props.scramble}
-    </Typography>
-  );
+class Scramble extends Component {
+
+ render() {
+   return (
+     <Typography
+       variant={"body1"}
+       color={"textSecondary"}
+       style={{ marginBottom: 80 }}
+     >
+       {this.props.scramble}
+     </Typography>
+   );
+ }
 }
+
 
 export default connect(state => ({
   scramble: state.scramble
