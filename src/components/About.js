8d472ebb1ea4b10a28cdac94688296d3d7bf0021
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2020-01-19 11:56:50 -0800

    Update About page

--- src/components/About.js
+++ src/components/About.js
@@ -10,16 +10,23 @@ export default function About() {
     <Card>
       <CardContent>
         <Typography variant={"body2"} color={"textSecondary"}>
-          This online timer was created to use the WCA's official scrambling algorithm from <Link
+          This online timer uses the WCA's official scrambling code from <Link
           href="https://github.com/thewca/tnoodle" target="_blank" rel="noopener noreferrer">
           TNoodle
         </Link>. Unfortunately, the latest release I was able to implement was version <Link
           href="https://github.com/thewca/tnoodle/releases/tag/v0.14.0" target="_blank" rel="noopener noreferrer">
           v0.14.0
         </Link> as more recent releases adopt a new build system that does not generate the
-          scrambling code in JavaScript. In the future, it may be worth it to manually port new releases into
-          JavaScript, but it does not seem necessary for the time being as it appears that the scrambling code is mature
-          and will not change drastically.
+          scrambling code in JavaScript.
+
+          <br/>
+          <br/>
+
+          The official scrambling code takes some time to generate scrambles, and it's more apparent with more complex puzzles like
+          7x7x7 and Square-1. While generating the scramble, nothing else can be done, and the entire site will appear to
+          be freezing up. This is expected behavior, and the timer attempts to mitigate this effect by displaying
+          the new scramble while the next one generates.
+
           <br/>
           <br/>
 
